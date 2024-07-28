bde03b3843d6bcb093bf6c42b665cc0c28876ed8
FaureWu <faure@wis.design>
2024-07-28 12:49:43 +0800

    feat(cli): support controlling the cross-terminal agent through global variables

---
+++ .changeset/forty-eggs-pay.md
@@ -0,0 +1,6 @@
+---
+'@wisdesign/cross-webpack-plugin': patch
+'@wisdesign/cli': patch
+---
+
+[feature] support controlling the cross-terminal agent through global variables
--- packages/cross-webpack-plugin/CrossWebpackPlugin.js
+++ packages/cross-webpack-plugin/CrossWebpackPlugin.js
@@ -20,7 +20,7 @@ class CrossWebpackPlugin {
           const rawSource = sourceMap.get('javascript')
 
           const code = rawSource.source()
-          if (code.includes(this.getMobileRegex())) {
+          if (code.includes('window.$$AGENT')) {
             return
           }
 
@@ -43,15 +43,15 @@ class CrossWebpackPlugin {
     })
   }
 
-  getMobileRegex() {
-    return '/Mobi|Android|iPhone/i'
-  }
-
   generatorAgentSource() {
     return `
       var agent = 'pc'
-      if (${this.getMobileRegex()}.test(navigator.userAgent)) {
+      if (/(android|bb\\d+|meego).+mobile|avantgo|bada\\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(navigator.userAgent || navigator.vendor || window.opera)) {
         agent = 'mobile'
+      }
+
+      if (window.$$AGENT) {
+        agent = window.$$AGENT
       }
 
       if (moduleMap[module + '/' + agent]) {
