bfe209407111c85cb64ebf9ecc030f1e1778cfaa
FaureWu <faure@wis.design>
2025-05-23 23:04:36 +0800

    feat(table-plugin): add the babel ast parser

--- packages/table-plugin/src/rsbuild/loaderTable.ts
+++ packages/table-plugin/src/rsbuild/loaderTable.ts
@@ -1,15 +1,45 @@
 // @ts-nocheck
 
-function loaderTable(source, ...rest) {
-  const { swc } = this._compiler.rspack.experiments
+import * as parser from "@babel/parser";
+import traverse from "@babel/traverse";
+import t from "@babel/types";
+// import * as generator from "@babel/generator";
 
-  // const ast = swc.parseSync(`
-  //   <Cell>
-  //     {{cellData.data}}
-  //   </Cell>  
-  // `);
+function loaderTable(source: string) {
+  if (
+    !this.resourcePath.endsWith(".tsx") &&
+    !this.resourcePath.endsWith(".jsx")
+  ) {
+    return source;
+  }
 
-  console.log(...rest)
+  const ast = parser.parse(
+    `import { Table, Cell } from "wis/table";
+
+    export default function () {
+      return (
+        <Cell role="cell">
+          {cellData.data}
+        </Cell>
+      )
+    }`,
+    {
+      sourceType: "module",
+      plugins: ["jsx", "typescript"],
+    },
+  );
+
+  traverse.default(ast, {
+    JSXElement(path: unknown) {
+      const openingElement = path.node.openingElement;
+      if (
+        t.isJSXIdentifier(openingElement.name) &&
+        openingElement.name.name === "Cell"
+      ) {
+      }
+    },
+  });
+
   process.exit();
 
   return source;
